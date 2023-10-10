018e8c04d777d7970da45b9f97048d62ba5c14c9
rasmus <rasmus.b.bodker@gmail.com>
2023-10-10 16:04:12 +0200

    Initial commit

---
+++ src/main/java/dk/dtu/CDIOg18/dicegame/App.java
@@ -0,0 +1,13 @@
+package dk.dtu.CDIOg18.dicegame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
