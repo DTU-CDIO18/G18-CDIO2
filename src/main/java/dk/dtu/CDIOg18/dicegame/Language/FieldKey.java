78a88fd36a272b5c36e3bc0f9e9f09a66c38c62e
Daniel Lerche <daniel.lerche10@gmail.com>
2023-10-27 13:15:00 +0200

    Added two new enums that hold the keys to some different language strings

---
+++ src/main/java/dk/dtu/CDIOg18/dicegame/Language/FieldKey.java
@@ -0,0 +1,29 @@
+package dk.dtu.dicegame.Language;
+
+public enum FieldKey implements LanguageKey {
+
+    TOWER("TOWER"),
+    CRATER("CRATER"),
+    PALACE_GATES("PALACE_GATES"),
+    COLD_DESERT("COLD_DESERT"),
+    WALLED_CITY("WALLED_CITY"),
+    MONASTERY("MONASTERY"),
+    BLACK_CAVE("BLACK_CAVE"),
+    HUTS_IN_THE_MOUNTAIN("HUTS_IN_THE_MOUNTAIN"),
+    WEREWOLF_WALL("WEREWOLF_WALL"),
+    THE_PIT("THE_PIT"),
+    GOLDMINE("GOLDMINE");
+
+    final String prefix = "Field";
+    String key;
+
+    FieldKey(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getKey() {
+        return prefix + "." + key;
+    }
+    
+}
